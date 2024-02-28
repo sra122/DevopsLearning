4bbc45508d46322566c06b2bb15d28ddfdf0a2fd
Sravan Kumar Katta <sr.katta@reply.de>
2024-02-28 20:52:52 +0100

    initial commit

---
+++ src/main/java/com/devops/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.devops.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/msg")
+    public ResponseEntity<String> message() {
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
+}
